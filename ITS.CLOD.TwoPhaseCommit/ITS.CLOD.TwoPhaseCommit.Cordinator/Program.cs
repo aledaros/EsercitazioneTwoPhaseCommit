59ce08d41223eb902a3f3c15c1d811df11627ae1
paolopiaia <paolo.piaia@stud.tecnicosuperiorekennedy.it>
2022-01-19 11:17:26 +0100

    add configuration masstransit

--- ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.Cordinator/Program.cs
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.Cordinator/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, rabbitConfigurator) =>
+    {
+        rabbitConfigurator.Host(
+            "localhost",
+            "/",
+            credentials =>
+            {
+                credentials.Username("guest");
+                credentials.Password("guest");
+            });
+    });
+});
+builder.Services.AddMassTransitHostedService();
+
 
 var app = builder.Build();
 
