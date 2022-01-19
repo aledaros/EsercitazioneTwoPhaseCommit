cb3208c8838a2c8bfef590227d1cd29fa490c871
paolopiaia <paolo.piaia@stud.tecnicosuperiorekennedy.it>
2022-01-19 14:17:02 +0100

    create entities order

---
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.Event/Enteties/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITS.CLOD.TwoPhaseCommit.Event.Enteties
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int Qty { get; set; }
+
+        public double Price { get; set; }
+
+
+    }
+}
