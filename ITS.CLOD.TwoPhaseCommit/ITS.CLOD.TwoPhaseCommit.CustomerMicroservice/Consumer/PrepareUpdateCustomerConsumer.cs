0d02d8554d97fb67c2d1c346a76f19a11f1cfd9e
paolopiaia <paolo.piaia@stud.tecnicosuperiorekennedy.it>
2022-01-19 13:20:38 +0100

    publish the event to send the answer to coordinator

--- ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.CustomerMicroservice/Consumer/PrepareUpdateCustomerConsumer.cs
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.CustomerMicroservice/Consumer/PrepareUpdateCustomerConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using ITS.CLOD.TwoPhaseCommit.CustomerMicroservice.Service;
+using ITS.CLOD.TwoPhaseCommit.Event;
 using ITS.CLOD.TwoPhaseCommit.Event.Enteties;
 using ITS.CLOD.TwoPhaseCommit.Event.Event;
 using MassTransit;
@@ -8,18 +9,27 @@ namespace ITS.CLOD.TwoPhaseCommit.CustomerMicroservice.Consumer
     public class PrepareUpdateCustomerConsumer : IConsumer<PrepareUpdateCustomerEvent>
     {
         public readonly CustomerService _customerService;
-
-        public PrepareUpdateCustomerConsumer(CustomerService customerService)
+        public readonly IPublishEndpoint _endpoint;
+        public PrepareUpdateCustomerConsumer(CustomerService customerService,IPublishEndpoint endpoint)
         {
             _customerService=customerService;
+            _endpoint=endpoint;
         }
 
-        public Task Consume(ConsumeContext<PrepareUpdateCustomerEvent> context)
+        public async Task Consume(ConsumeContext<PrepareUpdateCustomerEvent> context)
         {
             Customer customer = null;
             if (context.Message != null)
             {
                 customer= _customerService.GetCustomer(context.Message.CustomerId);
+            }
+            if(customer == null)
+            {
+                await _endpoint.Publish(new MessagePrepareEvent(context.Message.TransactionId,"Not ready",TransactionType.Customer));
+            }
+            else
+            {
+                await _endpoint.Publish(new MessagePrepareEvent(context.Message.TransactionId, "Ready", TransactionType.Customer));
             }
             
         }
