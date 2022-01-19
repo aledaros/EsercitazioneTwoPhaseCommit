3e9f6f552d0c8ead4c20f625f42f400b7e0bdfb0
paolopiaia <paolo.piaia@stud.tecnicosuperiorekennedy.it>
2022-01-19 12:33:52 +0100

    create customer repository

---
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.CustomerMicroservice/Repository/CustomerRepository.cs
@@ -0,0 +1,29 @@
+﻿using ITS.CLOD.TwoPhaseCommit.Event.Enteties;
+
+namespace ITS.CLOD.TwoPhaseCommit.CustomerMicroservice.Repository
+{
+    public class CustomerRepository
+    {
+
+
+        public readonly string _connectionString;
+        public readonly IConfiguration _configuration;
+        public CustomerRepository(IConfiguration configuration)
+        {
+            _configuration= configuration;
+            _connectionString=_configuration.GetConnectionString("AzureDb");
+        }
+
+
+        public Customer GetCustomer(int id)
+        {
+
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
