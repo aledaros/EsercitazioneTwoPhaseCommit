19bf21ed6e26d01dca0821aa3debf4bd4c319db6
paolopiaia <paolo.piaia@stud.tecnicosuperiorekennedy.it>
2022-01-19 12:32:08 +0100

    add rabbitmq to customer and order microservice

--- ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.CustomerMicroservice/Program.cs
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.CustomerMicroservice/Program.cs
@@ -1,4 +1,28 @@
+using ITS.CLOD.TwoPhaseCommit.Event.Event;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, rabbitConfigurator) =>
+    {
+        rabbitConfigurator.Host(
+            "localhost",
+            "/",
+            credentials =>
+            {
+                credentials.Username("guest");
+                credentials.Password("guest");
+            });
+        rabbitConfigurator.ReceiveEndpoint("prepareUpdateCustomerFund", e =>
+        {
+            e.Consumer<PrepareUpdateCustomer>();
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.OrderMicroservice/Program.cs
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.OrderMicroservice/Program.cs
@@ -1,4 +1,26 @@
+using ITS.CLOD.TwoPhaseCommit.Event.Event;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, rabbitConfigurator) =>
+    {
+        rabbitConfigurator.Host(
+            "localhost",
+            "/",
+            credentials =>
+            {
+                credentials.Username("guest");
+                credentials.Password("guest");
+            });
+        rabbitConfigurator.ReceiveEndpoint("prepareUpdateCustomerFund", e =>
+        {
+             e.Consumer<>();
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
