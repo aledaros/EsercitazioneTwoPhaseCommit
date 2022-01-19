51c033620a413735317f03b2a8567e75fb90dc79
paolopiaia <paolo.piaia@stud.tecnicosuperiorekennedy.it>
2022-01-19 14:49:17 +0100

    create order consumer

--- ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.OrderMicroservice/Consumer/PrepareUpdateOrderConsumer.cs
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.OrderMicroservice/Consumer/PrepareUpdateOrderConsumer.cs
@@ -1,13 +1,35 @@
-﻿using ITS.CLOD.TwoPhaseCommit.Event.Event;
+﻿using ITS.CLOD.TwoPhaseCommit.Event;
+using ITS.CLOD.TwoPhaseCommit.Event.Enteties;
+using ITS.CLOD.TwoPhaseCommit.Event.Event;
+using ITS.CLOD.TwoPhaseCommit.OrderMicroservice.Service;
 using MassTransit;
 
 namespace ITS.CLOD.TwoPhaseCommit.OrderMicroservice.Consumer
 {
     public class PrepareUpdateOrderConsumer : IConsumer<PrepareUpdateOrderEvent>
     {
-        public Task Consume(ConsumeContext<PrepareUpdateOrderEvent> context)
+        public readonly OrderService _orderService;
+        public readonly IPublishEndpoint _endpoint;
+        public PrepareUpdateOrderConsumer(OrderService orderService, IPublishEndpoint endpoint)
         {
-            throw new NotImplementedException();
+            _orderService=orderService;
+            _endpoint=endpoint;
+        }
+        public async Task Consume(ConsumeContext<PrepareUpdateOrderEvent> context)
+        {
+            Order order = null;
+            if(context.Message != null)
+            {
+                order= _orderService.Getorder(context.Message.OrderId);
+            }
+            if (order == null)
+            {
+                await _endpoint.Publish(new MessagePrepareEvent(context.Message.TransactionId, "Not ready", TransactionType.Order));
+            }
+            else
+            {
+                await _endpoint.Publish(new MessagePrepareEvent(context.Message.TransactionId, "Ready", TransactionType.Order));
+            }
         }
     }
 }
