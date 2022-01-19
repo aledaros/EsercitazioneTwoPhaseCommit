793ccb1045e8ac7845e05f0e3a48589cc2903b51
paolopiaia <paolo.piaia@stud.tecnicosuperiorekennedy.it>
2022-01-19 14:17:27 +0100

    create methods order service

---
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.OrderMicroservice/Service/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using ITS.CLOD.TwoPhaseCommit.Event.Enteties;
+using ITS.CLOD.TwoPhaseCommit.OrderMicroservice.Repository;
+
+namespace ITS.CLOD.TwoPhaseCommit.OrderMicroservice.Service
+{
+    public class OrderService
+    {
+        public readonly OrderRepository _orderRepository;
+
+        public OrderService(OrderRepository orderRepository)
+        {
+            _orderRepository=orderRepository;
+        }
+
+        public Order Getorder(int orderId)
+        {
+            return _orderRepository.GetOrder(orderId);
+
+        }
+
+        public void Insertorder(Order order)
+        {
+            _orderRepository.InsertOrder(order);
+        }
+
+        public void Updateorder(Order order)
+        {
+            _orderRepository.UpdateOrder(order);
+        }
+
+
+
+    }
+}
