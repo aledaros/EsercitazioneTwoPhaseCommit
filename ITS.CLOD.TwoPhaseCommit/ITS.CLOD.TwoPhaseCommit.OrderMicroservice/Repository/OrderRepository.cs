0a6eeefb3306f1762bde4323f85913fd5aad99c7
paolopiaia <paolo.piaia@stud.tecnicosuperiorekennedy.it>
2022-01-19 14:17:39 +0100

    create methods order repository

---
+++ ITS.CLOD.TwoPhaseCommit/ITS.CLOD.TwoPhaseCommit.OrderMicroservice/Repository/OrderRepository.cs
@@ -0,0 +1,42 @@
+﻿using Dapper.Contrib.Extensions;
+using ITS.CLOD.TwoPhaseCommit.Event.Enteties;
+using Microsoft.Data.SqlClient;
+
+namespace ITS.CLOD.TwoPhaseCommit.OrderMicroservice.Repository
+{
+    public class OrderRepository
+    {
+
+
+        public readonly string _connectionString;
+        public readonly IConfiguration _configuration;
+        public OrderRepository(IConfiguration configuration)
+        {
+            _configuration= configuration;
+            _connectionString=_configuration.GetConnectionString("AzureDb");
+        }
+
+
+        public Order GetOrder(int orderId)
+        {
+            Order order = null;
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                order=connection.Get<Order>(orderId);
+            }
+            return order;
+
+        }
+
+        public void InsertOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
