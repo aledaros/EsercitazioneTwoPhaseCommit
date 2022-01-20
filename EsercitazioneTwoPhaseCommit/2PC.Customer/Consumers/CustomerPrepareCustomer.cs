e639759edc1cf3e83776b8ac4de6fb13165e00c7
BolzanLuca <83642308+BolzanLuca@users.noreply.github.com>
2022-01-20 14:11:19 +0100

    .

---
+++ EsercitazioneTwoPhaseCommit/2PC.Customer/Consumers/CustomerPrepareCustomer.cs
@@ -0,0 +1,43 @@
+﻿using _2PC.Coordinator.DTO;
+using Dapper;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using _2PC.Customer.Entities;
+
+namespace _2PC.Customer.Consumers
+{
+    public class CustomerPrepareCustomer : IConsumer<PrepareUpdateCustomer>
+    {
+
+        string _connectionString = "";
+        public IConfiguration _configuration;
+        public CustomerPrepareCustomer(IConfiguration configuration)
+        {
+
+            _configuration = configuration;
+            _connectionString= _configuration.GetConnectionString("AzureDb");
+        }
+        public Task Consume(ConsumeContext<PrepareUpdateCustomer> context)
+        {
+
+
+            using var connection = new SqlConnection(_connectionString);
+
+            const string query = @"SELECT 
+      [fondo]
+  FROM[dbo].[Customer]
+where Id=@id";
+            var customerFund = connection.QueryAsync<Customers>(query, new
+            {
+                id = context.Message.Guid
+            });
+
+
+            return customerFund;
+
+        }
+    }
+}
+
